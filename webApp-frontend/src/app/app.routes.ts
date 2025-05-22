fafac14134112851df408ea03e6774930f4969d9
Jirawah <antoine.pouzeratte@hotmail.fr>
2025-05-22 15:35:32 +0200

    frontend 'patient' & 'patient-form' component added

--- webApp-frontend/src/app/app.routes.ts
+++ webApp-frontend/src/app/app.routes.ts
@@ -1,13 +1,52 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { PatientsComponent } from './components/patient/patient.component';
+// import { HomeComponent } from './home/home.component';
+// import { LoginComponent } from './auth/login/login.component';
+// import { RegisterComponent } from './auth/register/register.component';
+// import { PatientsComponent } from './components/patient/patient.component';
+
+// export const routes: Routes = [
+//   { path: '', title: 'Accueil', component: HomeComponent },
+//   { path: 'login', title: 'Connexion', component: LoginComponent },
+//   { path: 'register', title: 'Inscription', component: RegisterComponent },
+//   { path: 'patients', title: 'Patients', component: PatientsComponent },
+//   { path: '**', redirectTo: '' }
+// ];
 
 export const routes: Routes = [
-  { path: '', title: 'Accueil', component: HomeComponent },
-  { path: 'login', title: 'Connexion', component: LoginComponent },
-  { path: 'register', title: 'Inscription', component: RegisterComponent },
-  { path: 'patients', title: 'Patients', component: PatientsComponent },
-  { path: '**', redirectTo: '' }
+  {
+    path: '',
+    title: 'Accueil',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'login',
+    title: 'Connexion',
+    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    title: 'Inscription',
+    loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'patients',
+    title: 'Patients',
+    loadComponent: () => import('./components/patient/patient.component').then(m => m.PatientsComponent)
+  },
+  {
+    path: 'patients/new',
+    title: 'Ajouter un patient',
+    loadComponent: () => import('./components/patient-form/patient-form.component').then(m => m.PatientFormComponent)
+  },
+    {
+    path: 'patients/edit/:id',
+    loadComponent: () =>
+      import('./components/patient-form/patient-form.component').then(m => m.PatientFormComponent),
+    title: 'Modifier un patient'
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
+
--- webApp-frontend/src/app/components/patient/patient.component.ts
+++ webApp-frontend/src/app/components/patient/patient.component.ts
@@ -1,3 +1,55 @@
+// import { Component, OnInit } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { MatCardModule } from '@angular/material/card';
+// import { MatTableModule } from '@angular/material/table';
+// import { MatButtonModule } from '@angular/material/button';
+// import { PatientService } from '../../service/patient.service';
+// import { Patient } from '../../model/patient.model';
+// import { Router } from '@angular/router';
+// import { MatIconModule } from '@angular/material/icon';
+
+// @Component({
+//   selector: 'app-patient',
+//   standalone: true,
+//   imports: [
+//     CommonModule,
+//     MatCardModule,
+//     MatTableModule,
+//     MatButtonModule,
+//     MatIconModule
+//   ],
+//   templateUrl: './patient.component.html',
+//   styleUrls: ['./patient.component.scss']
+// })
+// export class PatientsComponent implements OnInit {
+//   patients: Patient[] = [];
+//   displayedColumns = ['prenom', 'nom', 'birthday', 'gender', 'phone', 'address', 'email', 'actions'];
+
+//   constructor(private patientService: PatientService, private router: Router) {}
+
+//   ngOnInit(): void {
+//     this.patientService.getAll().subscribe(data => {
+//       this.patients = data;
+//     });
+//   }
+
+//   onCreatePatient(): void {
+//   this.router.navigate(['/patients/new']);
+// }
+
+//   editPatient(id: number) {
+//     console.log('Edit', id);
+//   }
+
+//   manageNotes(id: number) {
+//     console.log('Manage notes for', id);
+//   }
+
+//   generateScore(id: number) {
+//     console.log('Generate score for', id);
+//   }
+// }
+
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
@@ -5,15 +57,18 @@ import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { PatientService } from '../../service/patient.service';
 import { Patient } from '../../model/patient.model';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-patients',
+  selector: 'app-patient',
   standalone: true,
   imports: [
     CommonModule,
     MatCardModule,
     MatTableModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './patient.component.html',
   styleUrls: ['./patient.component.scss']
@@ -22,7 +77,7 @@ export class PatientsComponent implements OnInit {
   patients: Patient[] = [];
   displayedColumns = ['prenom', 'nom', 'birthday', 'gender', 'phone', 'address', 'email', 'actions'];
 
-  constructor(private patientService: PatientService) {}
+  constructor(private patientService: PatientService, private router: Router) {}
 
   ngOnInit(): void {
     this.patientService.getAll().subscribe(data => {
@@ -30,15 +85,19 @@ export class PatientsComponent implements OnInit {
     });
   }
 
-  editPatient(id: number) {
-    console.log('Edit', id);
+  onCreatePatient(): void {
+    this.router.navigate(['/patients/new']);
   }
 
-  manageNotes(id: number) {
+  editPatient(id: number): void {
+    this.router.navigate(['/patients/edit', id]);
+  }
+
+  manageNotes(id: number): void {
     console.log('Manage notes for', id);
   }
 
-  generateScore(id: number) {
+  generateScore(id: number): void {
     console.log('Generate score for', id);
   }
 }
